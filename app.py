a08198454b290776d1e482e1c119fcc5ab55e1bf
imukeshkumarprajapa <imukeshkumarprajapat@gmail.com>
2025-11-05 23:37:29 +0530

    kamlesh kumar

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-def addition(a ,b, c):
-    return a + b+c
+def addition(a,b,c):
+    return a+b+c
 
 def substraction(a, b, c):
     return a - b-c
 
 def multiplication(a, b):
-    return a * b
+    return a * b 
+
+def division(a, b):
+    return a / b
+
+
+def modulus(a, b):
+    return a % b
